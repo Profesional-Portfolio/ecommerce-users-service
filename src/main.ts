fe3fac544e81d98851e0c278d18a04567b2ca7e5
Deivid182 <67056846+Deivid182@users.noreply.github.com>
2026-02-27 02:56:48 -0600

    feat: integrate RabbitMQ microservice and add environment configuration

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,47 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+
+import { env } from './modules/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  const logger = new Logger('Bootstrap');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: env.RABBITMQ_URLS,
+      queue: env.RABBITMQ_QUEUE,
+      queueOptions: {
+        durable: false,
+      },
+    }
+  })
+
+  // const app = await NestFactory.create(AppModule);
   
   // Habilitar CORS
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: true,
+  //   credentials: true,
+  // });
   
-  // Configurar validación global
+  // // Configurar validación global
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     whitelist: true,
     forbidNonWhitelisted: true,
   }));
   
-  const port = process.env.PORT || 3001;
-  await app.listen(port);
+  // const port = env.PORT;
+  // await app.listen(port);
+
+  await app.listen();
   
-  console.log(`🔐 User Service ejecutándose en puerto ${port}`);
-  console.log(`🗄️ Base de datos: ${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`);
+  // console.log(`🔐 User Service ejecutándose en puerto ${port}`);
+  // console.log(`🗄️ Base de datos: ${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`);
 }
 
 bootstrap();
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { LoginDto } from '../user/dto/login.dto';
@@ -7,9 +8,11 @@ import { LoginDto } from '../user/dto/login.dto';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('register')
-  async register(@Body() createUserDto: CreateUserDto) {
-    return this.authService.register(createUserDto);
+  // @Post('register')
+  @MessagePattern({ cmd: 'auth.register.user' })
+  async register(@Payload() createUserDto: CreateUserDto) {
+    return createUserDto;
+    // return this.authService.register(createUserDto);
   }
 
   @Post('login')
